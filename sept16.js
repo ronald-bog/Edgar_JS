0e3f26cb45c7c5a53dff7f6b2c1ee1296b453e3d
Ronald Espitia <ronald79@gmail.com>
2024-09-16 18:06:57 -0500

    Clase Sept 16

---
+++ sept16.js
@@ -0,0 +1,12 @@
+// prompt: recibir datos, inputs
+
+const nombre = prompt('DAME TU NOMBRE');
+
+console.log('Linea despues del metodo prompt');
+
+
+// alert: mostrar datos, resultados, outputs
+
+alert('Hola desde el ALERT');
+alert(`Como estas ${nombre}`);
+console.log('Linea despues del metodo ALERT');
